55ef0449fa49914db475a91aeb47edd1da0c4443
Arto Bendiken <arto@bendiken.net>
2010-06-18 01:53:51 +0200

    Bumped the version to 0.2.0.

--- lib/rdf/json/version.rb
+++ lib/rdf/json/version.rb
@@ -1,8 +1,8 @@
 module RDF; module JSON
   module VERSION
     MAJOR = 0
-    MINOR = 1
-    TINY  = 1
+    MINOR = 2
+    TINY  = 0
     EXTRA = nil
 
     STRING = [MAJOR, MINOR, TINY, EXTRA].compact.join('.')
