e7074308ee9594dee333e90f394fd434f349a039
Gregg Kellogg <gregg@kellogg-assoc.com>
2013-11-13 17:56:20 -0800

    Update for RDF.rb 1.1 and the RDF 1.1 Note on RDF/JSON.

--- Gemfile
+++ Gemfile
@@ -2,9 +2,16 @@ source "https://rubygems.org"
 
 gemspec :name => ""
 
-gem "rdf",      :git => "git://github.com/ruby-rdf/rdf.git"
-gem "rdf-spec", :git => "git://github.com/ruby-rdf/rdf-spec.git"
+gem "rdf",      :git => "git://github.com/ruby-rdf/rdf.git", :branch => "develop"
 
 group :development do
+  gem "rdf-spec", :git => "git://github.com/ruby-rdf/rdf-spec.git", :branch => "develop"
   gem "wirble"
+end
+
+group :debug do
+  gem "wirble"
+  gem "redcarpet", :platforms => :ruby
+  gem "debugger", :platforms => [:mri_19, :mri_20]
+  gem "ruby-debug", :platforms => :jruby
 end
--- spec/reader_spec.rb
+++ spec/reader_spec.rb
@@ -1,4 +1,7 @@
-require File.join(File.dirname(__FILE__), 'spec_helper')
+# coding: utf-8
+$:.unshift "."
+require 'spec_helper'
+require 'rdf/spec/reader'
 
 describe RDF::JSON::Reader do
   it "should be discoverable" do
@@ -82,6 +85,92 @@ describe RDF::JSON::Reader do
 
     it "should return the correct number of statements" do
       @reader.graph.count.should == @ntriples.count
+    end
+  end
+
+  context "Examples from spec" do
+    {
+      "example 1" => [
+        %q{<http://example.org/about> <http://purl.org/dc/terms/title> "Anna's Homepage"@en .},
+        %q{{
+          "http://example.org/about" : {
+              "http://purl.org/dc/terms/title" : [ { "value" : "Anna's Homepage", 
+                                                     "type" : "literal", 
+                                                     "lang" : "en" } ] 
+          }
+        }}
+      ],
+      "example 3" => [
+        %q{
+          <http://example.org/about> <http://purl.org/dc/terms/title> "Anna's Homepage"@en .                                                        
+          <http://example.org/about> <http://purl.org/dc/terms/title> "Annas hjemmeside"@da . 
+        },
+        %q{{
+          "http://example.org/about" : {
+              "http://purl.org/dc/terms/title" : [ { "value" : "Anna's Homepage", 
+                                                     "type" : "literal", 
+                                                     "lang" : "en" },
+                                                   { "value" : "Annas hjemmeside", 
+                                                     "type" : "literal", 
+                                                     "lang" : "da" } ] 
+          }
+        }}
+      ],
+      "example 5" => [
+        %q{
+          <http://example.org/about> <http://purl.org/dc/terms/title> "<p xmlns=\"http://www.w3.org/1999/xhtml\"><b>Anna's</b> Homepage>/p>"^^<http://www.w3.org/1999/02/22-rdf-syntax-ns#XMLLiteral> .
+        },
+        %q{{
+          "http://example.org/about" : {
+              "http://purl.org/dc/terms/title" : [ { "value" : "<p xmlns=\"http://www.w3.org/1999/xhtml\"><b>Anna's</b> Homepage>/p>", 
+                                                     "type" : "literal", 
+                                                     "datatype" : "http://www.w3.org/1999/02/22-rdf-syntax-ns#XMLLiteral" } ] 
+          }
+        }}
+      ],
+      "example 7" => [
+        %q{
+          <http://example.org/about> <http://purl.org/dc/terms/creator> _:anna .
+          _:anna <http://xmlns.com/foaf/0.1/name> "Anna" . 
+        },
+        %q({
+          "http://example.org/about" : {
+              "http://purl.org/dc/terms/creator" : [ { "value" : "_:anna",   "type" : "bnode" } ]
+          },
+          "_:anna" : {
+              "http://xmlns.com/foaf/0.1/name" : [ { "value" : "Anna",  "type" : "literal" } ] 
+          }
+        })
+      ],
+      "example 9" => [
+        %q{
+          _:anna <http://xmlns.com/foaf/0.1/homepage> <http://example.org/anna> .
+        },
+        %q{{
+          "_:anna" : {
+              "http://xmlns.com/foaf/0.1/homepage" : [ { "value" : "http://example.org/anna", 
+                                                         "type" : "uri" } ] 
+          }
+        }}
+      ],
+      "example 11" => [
+        %q{
+          _:anna <http://xmlns.com/foaf/0.1/name> "Anna" .
+          _:anna <http://xmlns.com/foaf/0.1/homepage> <http://example.org/anna> .        },
+        %q{{
+          "_:anna" : {
+              "http://xmlns.com/foaf/0.1/name" : [ { "value" : "Anna", 
+                                                     "type" : "literal" } ],
+              "http://xmlns.com/foaf/0.1/homepage" : [ { "value" : "http://example.org/anna", 
+                                                         "type" : "uri" } ] 
+          }
+        }}
+      ],
+    }.each do |test, (nt, json)|
+      it "parses #{test}", :pending => (test == 'example 7') do
+        g = RDF::Graph.new << RDF::JSON::Reader.new(json)
+        expect(g).to be_equivalent_graph(nt)
+      end
     end
   end
 end
